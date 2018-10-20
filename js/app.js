955b9e5bb26eca6a2b594085b9476d3dd91767b5
Rodolfo Perez <rodolfobeste@gmail.com>
2018-10-20 20:12:09 +0300

    Added responsiveness for tablet - desktop, and search bar functionality

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 const directory = document.querySelector("#directory");
 
 
-fetch("https://randomuser.me/api/?results=12&inc=name,email,location,dob,cell,picture")
+fetch("https://randomuser.me/api/?results=12&inc=name,email,location,dob,cell,picture&nat=us,dk,fr,gb,fi")
     
     .then(response => response.json())
     .then(response => create_user_card(response.results))
+    .then(capitalize_letter);
     
 
     function create_user_card (array) {
@@ -13,7 +14,7 @@ fetch("https://randomuser.me/api/?results=12&inc=name,email,location,dob,cell,pi
            <div class="card">
             <img src="${employee.picture.large}">
             <div class="info_wrapper">
-              <h3>${employee.name.first} ${employee.name.last}</h3>
+              <h3><span class="first_name">${employee.name.first}</span> ${employee.name.last}</h3>
               <p>${employee.email}</p>
               <p>${employee.location.state}</p>
             </div>
@@ -23,3 +24,30 @@ fetch("https://randomuser.me/api/?results=12&inc=name,email,location,dob,cell,pi
  
            directory.innerHTML = employees;
     }
+
+
+// SEARCH FUNCTIONALITY
+
+const search_field = document.querySelector('#search_employee');
+
+search_field.addEventListener("keyup", () => {
+  search_employee();
+})
+
+function search_employee() {
+  
+  const employee_to_search = search_field.value.toUpperCase();
+  const employee_name_h3 = document.querySelectorAll(".card h3");
+  
+  for (i = 0; i < employee_name_h3.length; i += 1) {
+     e = employee_name_h3[i];
+
+     if (e.textContent.toUpperCase().indexOf(employee_to_search) > -1) {
+      e.parentNode.parentNode.style.display = "";
+     } else {
+      e.parentNode.parentNode.style.display = "none";
+     }
+  }
+}
+
+
