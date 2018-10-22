2a122ae4615ce7918bb6b31dcbf940147f97331f
Rodolfo Perez <rodolfobeste@gmail.com>
2018-10-22 16:38:04 +0300

    Created functionality for creating modal cards and closing modal

--- js/app.js
+++ js/app.js
@@ -2,16 +2,18 @@
 // FETCH REQUEST TO RANDOM USER API
 
 const directory = document.querySelector("#directory");
+let employeesList = " "
 
 fetch("https://randomuser.me/api/?results=12&inc=name,email,location,dob,cell,picture&nat=us,dk,fr,gb,fi")
     
     .then(response => response.json())
     .then(response => create_employee_object(response.results))
-    .then(data => create_user_card(data));
+    .then(data => create_user_card(data))
+    .catch(error => directory.innerHTML = `There was an error: ${error}` );
 
       // Class for creating employee_details object
     class employee_details {
-      constructor (first_name, second_name, email, city, address, birthday, foto) {
+      constructor (first_name, second_name, email, city, address, birthday, foto, cell) {
         this.firstName = first_name;
         this.secondName = second_name;
         this.email = email;
@@ -19,6 +21,7 @@ fetch("https://randomuser.me/api/?results=12&inc=name,email,location,dob,cell,pi
         this.address = address;
         this.birthday = birthday;
         this.foto = foto; 
+        this.cell = cell;
       }
     }
 
@@ -31,8 +34,9 @@ fetch("https://randomuser.me/api/?results=12&inc=name,email,location,dob,cell,pi
 
         const a = array[i];
          
-        employees_list.push(new employee_details(a.name.first, a.name.last, a.email, a.location.state, a.location.street, a.dob.date, a.picture.large));
+        employees_list.push(new employee_details(a.name.first.charAt(0).toUpperCase() + a.name.first.slice(1), a.name.last.charAt(0).toUpperCase() + a.name.last.slice(1), a.email, a.location.state.charAt(0).toUpperCase() + a.location.state.slice(1), a.location.street, a.dob.date.slice(-10), a.picture.large, a.cell));
       }
+      employeesList = employees_list;
       return employees_list
     }
 
@@ -40,14 +44,16 @@ fetch("https://randomuser.me/api/?results=12&inc=name,email,location,dob,cell,pi
     function create_user_card (array) {
       const employees = array.map(employee =>
         `
-        <div class="card">
-         <img src="${employee.foto}">
-         <div class="info_wrapper">
-           <h3><span class="first_name">${employee.firstName}</span> ${employee.secondName}</h3>
-           <p>${employee.email}</p>
-           <p>${employee.city}</p>
-         </div>
-       </div>
+        <div class="card"> 
+            <img src="${employee.foto}">
+              <div class="info_wrapper">
+                <h3>${employee.firstName} ${employee.secondName}</h3>
+                <p>${employee.email}</p>
+                <p>${employee.city}</p>
+              </div>
+          <div class="card_overlay">
+          </div>
+        </div>
        `
         ).join("");
 
@@ -77,6 +83,70 @@ function search_employee() {
       e.parentNode.parentNode.style.display = "none";
      }
   }
+}
+
+directory.addEventListener("click", (event) => {
+  const e = event.target;
+  const cards = document.querySelectorAll(".card");
+  let number;
+
+  function card_index (array) {
+    for(let i = 0; i < array.length; i += 1){
+        
+      if (array[i].textContent === e.parentNode.textContent) {
+        number = i;
+      }
+    }
+  }
+
+
+
+  if (e.className == "card_overlay") {
+    
+    card_index(cards);
+    create_modal_card(number);
+  }
+});
+
+const modal_section = document.querySelector("#modal_section");
+
+
+modal_section.addEventListener("click", (event) => {
+  const modal = document.querySelector(".modal");
+  const modal_card = document.querySelector(".modal_card");
+
+  if (event.target.className === "modal") {
+    modal.style.display = "none";
+    modal_card.style.display = "none";
+  }
+  
+})
+
+
+function create_modal_card (number) {
+
+  const c =
+  `
+  <div class="modal_card">
+    <p class="close_modal">X</p>
+    <div>
+      <img src="${employeesList[number].foto}">
+      <h3>${employeesList[number].firstName} ${employeesList[number].secondName}</h3>
+      <p>${employeesList[number].email}</p>
+      <p>${employeesList[number].city}</p>
+    </div>
+    <div>
+      <p>${employeesList[number].cell}</p>
+      <p>${employeesList[number].address}</p>
+      <p>Birthday: ${employeesList[number].birthday}</p>
+    </div>
+  </div>
+
+  <div class="modal">
+  </div>
+  `;
+modal_section.innerHTML = c;
+  
 }
 
 
